847808aef738f1217cf4a53a5008f7977a581906
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-06-17 16:23:17 +0530

    update

--- support_server.js
+++ support_server.js
@@ -3,17 +3,26 @@
                   const {DataBaseconnection} = require("./Database/DBconnection")
                    const errorMiddliware=require("./middileware/errorMiddileware");
                    const cookieParser = require("cookie-parser");
+                   const fileUpload = require("express-fileupload");
+                   const bodyParser = require("body-parser");
                   const support_server=express();
+                  const cloudinary = require("cloudinary")
                              
                         // middileware for json
                          support_server.use(express.json());
                          support_server.use(cookieParser());
-
+                         support_server.use(bodyParser.urlencoded({ extended: true }));
+                         support_server.use(fileUpload());
                         
 
                        // database connect come on sourav
                        DataBaseconnection();
-
+                         // card pdf files upload on cloudinary
+                         cloudinary.config({
+                          cloud_name: process.env.CLOUDINARY_NAME,
+                          api_key: process.env.CLOUDINARY_API_KEY,
+                          api_secret: process.env.CLOUDINARY_API_SECRET,
+                        });
 
                      // routes path home 
                      support_server.get('/',(req,res)=>{
