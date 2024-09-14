4312632a0547f17b8a8bd38b88bf80aaed67684f
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-09-14 22:22:46 +0530

    Update jwtToken.js

--- special/jwtToken.js
+++ special/jwtToken.js
@@ -5,7 +5,9 @@ const sendToken = (user, statuscode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 // cookie expire time 24hours * 7
     ),
-    httpOnly: true,
+    httpOnly: true, // Prevents JavaScript access to the cookie
+    secure: process.env.NODE_ENV === 'production', // Ensure cookie is sent over HTTPS in production
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // Allow cross-origin requests in production
   };
 
   res.status(statuscode).cookie("token", token, options).json({
@@ -15,4 +17,5 @@ const sendToken = (user, statuscode, res) => {
     token,
   });
 };
+
 module.exports = sendToken;
