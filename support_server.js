9543baf8ea7fa33944250e3bb57865be66228920
souravcsewala <159464035+souravcsewala@users.noreply.github.com>
2024-09-14 21:41:09 +0530

    Update support_server.js

--- support_server.js
+++ support_server.js
@@ -21,7 +21,7 @@ support_server.use(
 );
 // cors policy
  const corsOptions = {
-   origin: ["http://localhost:5173", "http://localhost:3000", "https://www.supplyprovision.com/", "https://supplyprovision.com/"],
+   origin: ["http://localhost:5173", "http://localhost:3000", "https://www.supplyprovision.com", "https://supplyprovision.com"],
    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
    credentials: true,
 };
